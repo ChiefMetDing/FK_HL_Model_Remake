7f47aa52a38456d13275e95c28230bfaba8884df
Ding Zhang <Ding.Zhang@Kinross.com>
2021-07-21 21:28:22 -0400

    updating files

--- Scripts/static/js/mapping.js
+++ Scripts/static/js/mapping.js
@@ -1,12 +1,17 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
+
+
+
+
+// -----------------CREATE MAP--------------------------------------------------
 const mapCenter=[65.018, -147.36]//the center of the pad
 const zoomLevel=15
 const mapID='mapid'
 
 // We create the tile layer that will be the background of our map.
-// We create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
 		{
 			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -24,5 +29,25 @@ let map = L.map(mapID, {
 	zoom: zoomLevel,
 	layers: [satelliteStreets]
 });
+
+let cells = "../../../Resources/cells_map.geojson"
+
+let myStyle = {
+    color:"blue",
+    fillColor:"yellow",
+    weight:1
+}
+
+d3.json(cells).then(function(data){
+	console.log(data)
+	L.geoJSON(data,{
+		onEachFeature:function(feature,layer){
+			layer.bindPopup("<h2>Area Name: " + feature.properties.cell + "</h2><hr><h3>Lift: " + feature.properties.lift + "</h3>");
+		},
+		style: myStyle
+	}).addTo(map)
+})
+
+
 
 L.control.layers(baseMaps).addTo(map);
